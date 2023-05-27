8eb9c288077a15bcb876557bf2a9714c10748c32
pavanh <pavanhdsce@gmail.com>
2023-05-27 08:50:41 +0530

    intial setup added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PaymentSuccess from "./pages/PaymentSuccess";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Pay from "./pages/Pay";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/pay" element={<Pay />} />
+        <Route path="/success" element={<PaymentSuccess />} />
+    </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/z6hmhCX/flower-removebg-preview.png",
+    title: "SUMMER SALE",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "C4DFDF",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/KGrScgb/assortment-leaves-flowers-white-background-removebg-preview.png",
+    title: "BLOOMING SALE",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "C4DFDF",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+    title: "FOR LOVE",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "C4DFDF",
+  },
+];
+
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+    title: "ANOTH",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+    title: "ROSE",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+    title: "JASMINE",
+  },
+];
+export const popularproducts = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QDBtFFq/3419379-removebg-preview.png",
+  },
+];
